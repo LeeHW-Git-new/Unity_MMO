5180e93f36cb2ec0f19fd776933b5f0e6d1c181d
Lee <aadd306@gmail.com>
2023-07-04 21:58:44 +0900

    Create GameManager , InputManager

---
+++ MMO_Unity/Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    static Manager s_instance;
+    static Manager Instance { get { Init(); return s_instance; } }
+    
+
+    InputManager _input = new InputManager();
+    public static InputManager Input { get { return Instance._input; } }
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        _input.OnUpdate();
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Manager>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Manager>();
+        }
+    }
+}
+
